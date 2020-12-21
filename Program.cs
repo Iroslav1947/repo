387d67500cf3ebfe4734ef337283b1e53c745869
Iroslav1947 <74483439+Iroslav1947@users.noreply.github.com>
2020-12-21 17:19:27 +0500

    Сообщение
    Описание ООП и Модульного програмирования

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repo
+{
+    class Program
+    {
+        class Point
+        {
+            public int x
+            public int y 
+            public char sym
+        }
+
+        static void Main(string[] args)
+        {
+            Point p1 = new Point()
+            Draw(p1..x, p1..y, p1..sym);
+            Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+           
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+        class Point 
+        {
+            int x;
+            int y;
+            char sym;
+        }
+
+    }
+}
